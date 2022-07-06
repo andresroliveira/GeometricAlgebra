da635d18de54cc237025b501303df172f0ff22d1
Andrês <andresroliveira98@gmail.com>
2022-07-06 17:24:08 -0300

    Add methods EuclideanVector

--- EuclideanVector.cpp
+++ EuclideanVector.cpp
@@ -1,6 +1,8 @@
 #include "EuclideanVector.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <stdexcept>
 
 EuclideanVector::EuclideanVector(){
     this->n = 3;
@@ -23,11 +25,20 @@ EuclideanVector::EuclideanVector(int *u){
     }
 }
 
+EuclideanVector::EuclideanVector(int dim){
+    this->n = dim;
+    this->v = (float*) calloc(n, sizeof(float));
+}
+
 EuclideanVector::~EuclideanVector(){
     free(v);
 }
 
 int EuclideanVector::length(){
+    return n;
+}
+
+int EuclideanVector::dim(){
     return n;
 }
 
@@ -37,4 +48,65 @@ void EuclideanVector::print(){
         printf(", %.2lf", v[i]);
     }
     printf("]\n");
+}
+
+bool EuclideanVector::isNull(){
+    for(int i=0;i<n;i++){
+        if(abs(v[i]) >= EPS) return false;
+    }
+    return true;
+}
+
+float EuclideanVector::norm(){
+    float s = 0.0;
+
+    for(int i=0;i<n;i++){
+        s += v[i]*v[i];
+    }
+
+    return sqrt(s);
+}
+
+void EuclideanVector::normalize(){
+    if(isNull()){
+        throw std::invalid_argument("Null vector.");
+    }
+
+    float nr = norm();
+
+    for(int i=0;i<this->n;i++){
+        setValueIndex(i, nr*getValueIndex(i));
+    }
+}
+
+float EuclideanVector::getValueIndex(int id){
+    return v[id];
+}
+
+void EuclideanVector::setValueIndex(int id, float value){
+    this->v[id] = value;
+}
+
+EuclideanVector EuclideanVector::sum(EuclideanVector obj){
+    if(length() != obj.length()){
+        throw std::invalid_argument("Not same dim.");
+    }
+
+    EuclideanVector out = EuclideanVector(obj.length());
+
+    for(int i=0;i<this->n;i++){
+        out.setValueIndex(i, getValueIndex(i) + obj.getValueIndex(i));
+    }
+
+    return out;
+}
+
+EuclideanVector EuclideanVector::multiply(float scalar){
+    EuclideanVector out = EuclideanVector(length());
+
+    for(int i=0;i<this->n;i++){
+        out.setValueIndex(i, scalar*getValueIndex(i));
+    }
+
+    return out;
 }
--- EuclideanVector.h
+++ EuclideanVector.h
@@ -8,12 +8,26 @@ public:
     EuclideanVector();
     EuclideanVector(float*);
     EuclideanVector(int*);
+    EuclideanVector(int);
     ~EuclideanVector();
     int length();
+    int dim();
     void print();
+    bool isNull();
+    float norm();
+    void normalize();
+
+    // Get & Set
+    float getValueIndex(int);
+    void setValueIndex(int, float);
+
+    // operators
+    EuclideanVector sum(EuclideanVector);
+    EuclideanVector multiply(float);
 private:
     int n;
     float *v;
+    const float EPS = 1e-9;
 };
 
 #endif
