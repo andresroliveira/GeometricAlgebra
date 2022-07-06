9c20285b77f9017538b4a771d848f0abfa0a7d78
Andrês <andresroliveira98@gmail.com>
2022-07-06 17:30:37 -0300

    Update main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 int main(){
     EuclideanVector v = EuclideanVector();
+    // v.print();
+    int U[] = {1, 2, 3};
+    EuclideanVector u = EuclideanVector(U);
+    u.print();
+    EuclideanVector w = v.sum(u);
+    w.print();
     v.print();
     return 0;
 }
